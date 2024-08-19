07cd1eceb10dc903053d6340f2ec2b3038e4b26d
kow111 <chome710@gmail.com>
2024-08-19 14:23:10 +0700

    add env file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+require("dotenv").config();
+const port = process.env.PORT || 8888;
 
+console.log(process.env);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
