7b6c8d3e27bfae2f9027d9c54f6aa04c5ecec48e
kow111 <chome710@gmail.com>
2024-08-26 17:09:50 +0700

    connect to mysql

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 require("dotenv").config();
 
+const mysql = require("mysql2");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8888;
 const webRoute = require("./routes/web");
 
 app.use("/", webRoute);
+
+//test conn
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+connection.query("SELECT * FROM Users u", function (err, results) {
+  console.log(results);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
