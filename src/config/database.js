9f64ef6d58ec25a0ce63bfd80e7e13bdb50729c2
kow111 <chome710@gmail.com>
2024-08-27 13:47:45 +0700

    connection pool pattern

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,22 @@
 const mysql = require("mysql2");
 //test conn
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3307,
+//   user: "root",
+//   password: "123456",
+//   database: "hoidanit",
+// });
+
+const connection = mysql.createPool({
   host: "localhost",
   port: 3307,
   user: "root",
   password: "123456",
   database: "hoidanit",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = connection;
