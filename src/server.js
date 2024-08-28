180c5e109c98a564fd4bfb5eb5d1a96e9c9722c3
kow111 <chome710@gmail.com>
2024-08-28 17:39:12 +0700

    setup express-fileupload

--- src/server.js
+++ src/server.js
@@ -5,9 +5,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8888;
 const apiRoute = require("./routes/api");
+const fileUpload = require("express-fileupload");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 app.use("/api/v1/user", apiRoute);
 
